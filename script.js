353867a11a9f87d1b5b1b6ea9839f80f14e6e3bc
DeBRIS <85679088+de-bris@users.noreply.github.com>
2021-06-20 12:11:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('sections')[0]
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active')
+})
+
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+TypeWriter.prototype.type = function() {
+    const current = this.wordIndex % this.words.length;
+
+    const fullTxt = this.words[current];
+
+    if(this.isDeleting) {
+
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+
+    } else {
+
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    setTimeout( () => this.type(), 120)
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    new TypeWriter(txtElement, words, wait);
+}
